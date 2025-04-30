debc374a156d822a5c5387d913b1cc25767a7bd1
robiverdev <robi.verdev1@gmail.com>
2025-04-30 17:46:31 +0200

    Added form and fetching user input

--- src/components/learning-resources/Resources.vue
+++ src/components/learning-resources/Resources.vue
@@ -10,7 +10,9 @@
       Add Resource
     </base-btn>
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -44,6 +46,7 @@
     provide() {
       return {
         resources: this.storedResources,
+        addResource: this.addResource,
       };
     },
     computed: {
@@ -57,6 +60,16 @@
     methods: {
       setSelectedTab(tab) {
         this.selectedTab = tab;
+      },
+      addResource(title, description, url) {
+        const newResource = {
+          id: new Date().toISOString(),
+          title: title,
+          description: description,
+          link: url,
+        };
+        this.storedResources.unshift(newResource);
+        this.selectedTab = 'stored-resources';
       },
     },
   };
