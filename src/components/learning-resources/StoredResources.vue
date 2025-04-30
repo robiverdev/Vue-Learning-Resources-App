ef2ef541e027834a96a88fa36f1607fd7cffd28b
robiverdev <robi.verdev1@gmail.com>
2025-04-30 11:25:39 +0200

    Added new components

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <ResourceItem
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></ResourceItem>
+  </ul>
+</template>
+
+<script>
+import ResourceItem from './ResourceItem.vue';
+
+export default {
+  props: ['resources'],
+  components: {
+    ResourceItem,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
